77f5c66fc0f9133ac12c67e838073df509d42a5b
thejassk7 <thejassuvarnak@gmail.com>
2026-01-23 08:39:12 +0530

    Added solution to third problem

--- Solution1.py
+++ Solution1.py
@@ -64,7 +64,7 @@ while i<n:
         g=g+1
         i=i+1
     elif word[i]=='H':
-        m=m+1
+        h=h+1
         i=i+1
     elif word[i]=='I':
         i1=i1+1
@@ -172,3 +172,112 @@ while i<n:
         print("The character y has come: ",y,"times.")
     if z>0:
         print("The character z has come: ",z,"times.")
+print("Question 3: String encoding: Perform basic string compression using the counts of repeated characters.")
+word=input("Enter a word to perform basic string compression using the counts of repeated characters")
+word=word.upper()
+n=len(word)
+i=0
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+i=0
+j=0
+k=0
+l=0
+m=0
+n=0
+o=0
+p=0
+q=0
+r=0
+s=0
+t=0
+u=0
+v=0
+w=0
+x=0
+y=0
+z=0
+while i<n:
+    if word[i]=='A':
+        a=a+1
+        i=i+1
+    if word[i]=='B':
+        b=b+1
+        i=i+1
+    if word[i]=='C':
+        c=c+1
+        i=i+1
+    if word[i]=='D':
+        d=d+1
+        i=i+1
+    if word[i]=='E':
+        e=e+1
+        i=i+1
+    if word[i]=='F':
+        f=f+1
+        i=i+1
+    if word[i]=='G':
+        g=g+1
+        i=i+1
+    if word[i]=='H':
+        h=h+1
+        i=i+1
+    if word[i]=='I':
+        i1=i1+1
+        i=i+1
+    if word[i]=='J':
+        j=j+1
+        i=i+1
+    if word[i]=='K':
+        k=k+1
+        i=i+1
+    if word[i]=='L':
+        l=l+1
+        i=i+1
+    if word[i]=='M':
+        m=m+1
+        i=i+1
+    if word[i]=='N':
+        n=n+1
+        i=i+1
+    if word[i]=='O':
+        o=o+1
+        i=i+1
+    if word[i]=='P':
+        p=p+1
+        i=i+1
+    if word[i]=='Q':
+        q=q+1
+        i=i+1
+    if word[i]=='R':
+        r=r+1
+        i=i+1
+    if word[i]=='S':
+        s=s+1
+        i=i+1
+    if word[i]=='T':
+        t=t+1
+        i=i+1
+    if word[i]=='U':
+        u=u+1
+        i=i+1
+    if word[i]=='V':
+        v=v+1
+        i=i+1
+    if word[i]=='W':
+        w=w+1
+        i=i+1
+    if word[i]=='X':
+        x=x+1
+        i=i+1
+    if word[i]=='Z':
+        z=z+1
+        i=i+1
+    print("a",a,"b",b,"c",c,"d",d,"f",f,"g",g,"h",h,"i",i,"j",j,"k",k,"l",l,"m",m,"n",n,"o",o,"p",p,"q",q,"r",r,"s",s,"t",t,"u",u,"v",v,"w",w,"x",x,"y",y,"z",z)
+    
